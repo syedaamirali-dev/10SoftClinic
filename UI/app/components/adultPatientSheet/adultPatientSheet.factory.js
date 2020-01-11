4af834e3e7857375d2acada9295c0373b90a88ef
Imadullah Sharief <imad.sharief@xitsolutionsinc.com>
2020-01-12 00:30:51 +0530

    Added New Component Adult Patient Screen

---
+++ UI/app/components/adultPatientSheet/adultPatientSheet.factory.js
@@ -0,0 +1,18 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('10softdental')
+        .factory('adultPatientSheetService', adultPatientSheetService)
+
+    /** @ngInject */
+    function adultPatientSheetService(apiCollection, $q, $http){
+        var service = {
+
+        };
+
+        return service;
+    }
+
+}());
+    
