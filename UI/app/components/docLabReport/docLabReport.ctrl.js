f3d88a4927ead4ebfa64b8b7adbd13442b1d8597
Imadullah Sharief <imad.sharief@xitsolutionsinc.com>
2020-02-03 11:50:31 +0530

    Adult Patient Lab Report Changes

--- UI/app/components/docLabReport/docLabReport.ctrl.js
+++ UI/app/components/docLabReport/docLabReport.ctrl.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     'use strict';
 
     angular
@@ -6,12 +6,95 @@
         .controller('docLabReportCtrl', docLabReportCtrl)
 
     /** @ngInject */
-    function docLabReportCtrl($scope, docLabReportService){
-        activate();
+    function docLabReportCtrl($scope, $timeout, docLabReportService) {
+        $scope.labReport = {
+            dtOptions: [],
+            isLoaded: false
+        };
+        $scope.labReportInfo =
+        {
+            labRequestList: [],
+            reportTypeList: [],
+            prefferedLabList: [],
+            gridData: [],
+            reset: () => {
+                $scope.labReportInfo.lrRequest = null;
+                $scope.labReportInfo.reportType = null;
+                $scope.labReportInfo.preferredLab = null;
+                $scope.labReportInfo.comments = null;
+            },
+            Submit: () => {
 
-        function activate(){
+                $scope.labReportInfo.gridData.push({
+                    lrRequest: $scope.labReportInfo.lrRequest,
+                    reportType: $scope.labReportInfo.reportType,
+                    reportTypeName: $scope.labReportInfo.reportTypeList.filter((item) => { return item.id == $scope.labReportInfo.reportType })[0].name,
+                    preferredLab: $scope.labReportInfo.preferredLab,
+                    comments: $scope.labReportInfo.comments,
+                    reportStatus: "Requested",
+                    report: null
+                });
+                $scope.labReportInfo.reset();
+                getLabReportData();
+                swal.fire({
+                    title: "Data Submitted",
+                    text: "Data Saved Successfully!",
+                    type: "success",
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+            }
         }
+
+        /**
+         * @description Get Lab Report Grid Data
+         */
+        function getLabReportData() {
+            $scope.labReport.isLoaded = false;
+            // docLabReportService.getLabReportData().then((data) => {
+            $timeout(() => {
+                $scope.labReportInfo.labRequestList = [
+                    {
+                        id: "LR"
+                    },
+                ];
+                $scope.labReportInfo.reportTypeList = [
+                    {
+                        id: "BR",
+                        name: "Blood Report"
+                    },
+                    {
+                        id: "Dental",
+                        name: "Dental Report"
+                    },
+                    {
+                        id: "Inventory",
+                        name: "Inventory Report"
+                    },
+                ];
+                $scope.labReportInfo.prefferedLabList = [
+                    {
+                        id: "Instrument",
+                    },
+                    {
+                        id: "Medical Laboratory",
+                    },
+                    {
+                        id: "Pathology Lab",
+                    },
+                ];
+                $scope.labReport.dtOptions = docLabReportService.getDtOptions($scope.labReportInfo.gridData, {});
+                $scope.labReport.isLoaded = true;
+            }, 1500);
+
+            // });
+        }
+
+
+        function activate() {
+            getLabReportData();
+        }
+        activate();
     }
 
 }());
-      
--- UI/app/components/docLabReport/docLabReport.factory.js
+++ UI/app/components/docLabReport/docLabReport.factory.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     'use strict';
 
     angular
@@ -6,13 +6,47 @@
         .factory('docLabReportService', docLabReportService)
 
     /** @ngInject */
-    function docLabReportService(apiCollection, $q, $http){
+    function docLabReportService(apiCollection, $q, $http) {
         var service = {
-
+            getLabReportData,
+            getDtOptions
         };
+        function httpGet(url) {
+            let defer = $q.defer();
+            $http.get(url).then(function (response) {
+                defer.resolve(response.data);
+            });
+            return defer.promise;
+        }
+
+        function httpPost(url, data) {
+            let defer = $q.defer();
+            $http.post(url, data).then(function (response) {
+                defer.resolve(response.data);
+            });
+            return defer.promise;
+        }
+        function getDtOptions(data, functions) {
+            return {
+                data: data,
+                columns: [
+                    { title: 'Lab Report Request #', data: 'lrRequest' },
+                    { title: 'Report Type', data: 'reportType' },
+                    { title: 'Lab Report Type Name', data: 'reportTypeName' },
+                    { title: 'Comment', data: 'comments' },
+                    { title: 'Preferrd Lab', data: 'preferredLab' },
+                    { title: 'Report Status', data: 'reportStatus' },
+                    { title: 'Show Report', data: 'report' },
+
+                ],
+                columnDefs: []
+            }
+        }
+        function getLabReportData() {
+            return httpGet(apiCollection.doctorTreatment.GetVisitHistory(), true);
+        }
 
         return service;
     }
 
 }());
-    
