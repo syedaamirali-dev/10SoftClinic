485b21c8fcf30c23a5c29302be3cbbdd0f21bcbf
Mohammed Azhar Uddin <mohammedazharuddin100@gmail.com>
2020-02-21 04:30:30 +0530

    API URL

--- 10SoftDentalAPI/10SoftDental/Controllers/TransactionController.cs
+++ 10SoftDentalAPI/10SoftDental/Controllers/TransactionController.cs
@@ -27,6 +27,7 @@ namespace _10SoftDental.Controllers
             }
         }
 
+        //http://localhost:55453/api/Transaction/GetAdultMainScreeningData?clinicId=3&patientId=15&Mobile=&doctorId=&DoctorTreatmentId=35&dentalMainId=1
         [HttpGet]
         public IHttpActionResult GetAdultMainScreeningData(int? clinicId, long patientId, string Mobile, long? doctorId, long? DoctorTreatmentId, long? dentalMainId)
         {
