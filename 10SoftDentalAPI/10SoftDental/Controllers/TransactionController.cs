0f33c0548ce7a11c804018079b617cb433dc5ac6
Mohammed Azhar Uddin <mohammedazharuddin100@gmail.com>
2020-02-28 01:31:12 +0530

    Update TransactionController.cs

--- 10SoftDentalAPI/10SoftDental/Controllers/TransactionController.cs
+++ 10SoftDentalAPI/10SoftDental/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Web.Http;
 using _10SoftDental.BAL.Dental;
@@ -11,6 +12,7 @@ namespace _10SoftDental.Controllers
         PatientAdultMain patientBAL = null;
         AdultMainTreatment adultMainTreatmentBAL = null;
         DataSet dataSet = null;
+        private List<PatientMedication> patientMedicationList = null;
 
         [HttpPost]
         public IHttpActionResult SaveDentalAdultMain(PatientAdultMain adultMain)
@@ -41,6 +43,7 @@ namespace _10SoftDental.Controllers
                 throw;
             }
         }
+
         //http://localhost:55453/api/Transaction/GetAdultMainScreeningData?clinicId=3&patientId=15&Mobile=&doctorId=&DoctorTreatmentId=35&dentalMainId=1
         [HttpGet]
         public IHttpActionResult GetAdultMainScreeningData(int? clinicId, long patientId, string Mobile, long? doctorId, long? DoctorTreatmentId, long? dentalMainId)
@@ -74,6 +77,88 @@ namespace _10SoftDental.Controllers
             }
         }
 
+        [HttpPost]
+        public IHttpActionResult SavePatientMedication(List<PatientMedication> patientMedicationList)
+        {
+            try
+            {
+                patientBAL = new PatientAdultMain();
+                patientBAL.SavePatientMedication(patientMedicationList);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult SavePatientCaseSheet(PatientAdultMain patientAdultMain)
+        {
+            try
+            {
+                patientBAL = new PatientAdultMain();
+                var result= patientAdultMain.SavePatientCaseSheet();
+                if (result == null) return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult SendforCaseStudy(PatientAdultMain patientAdultMain)
+        {
+            try
+            {
+                patientBAL = new PatientAdultMain();
+                var result = patientAdultMain.SendforCaseStudy();//Convert.ToInt64(patientAdultMain.DentalAdultMainId),Convert.ToBoolean(patientAdultMain.IsSentforCaseStudy)
+                if (result == null) return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult ApproveCaseStudy(PatientAdultMain patientAdultMain)
+        {
+            try
+            {
+                patientBAL = new PatientAdultMain();
+                var result = patientAdultMain.ApproveCaseStudy();//Convert.ToInt64(patientAdultMain.DentalAdultMainId),Convert.ToBoolean(patientAdultMain.IsSentforCaseStudy)
+                if (result == null) return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult SendforApproval(PatientAdultMain patientAdultMain)
+        {
+            try
+            {
+                patientBAL = new PatientAdultMain();
+                var result = patientAdultMain.SendforApproval(Convert.ToInt64(patientAdultMain.DentalAdultMainId),Convert.ToBoolean(patientAdultMain.IsSentForApproval));
+                if (result == null) return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
         [HttpGet]
         public IHttpActionResult GetDentalAdultTreatmentDetails(long? DentalTreatmentId)
         {
@@ -89,6 +174,40 @@ namespace _10SoftDental.Controllers
                 dataSet.Tables[4].TableName = "DoctorTreatmentIllness";
                 dataSet.Tables[5].TableName = "DoctorTreatmentSymptom";
                 dataSet.Tables[6].TableName = "DoctorTreatmentPatientProblem";
+                return Ok(dataSet);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetPatientMedication(long dentalAdultMainId)
+        {
+            try
+            {
+                patientMedicationList = new List<PatientMedication>();
+                patientBAL = new PatientAdultMain();
+                patientMedicationList = patientBAL.Dental_PatientMedication(dentalAdultMainId);
+                return Ok(patientMedicationList);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetPatienDropdownData()
+        {
+            try
+            {
+                dataSet = new DataSet();
+                patientBAL = new PatientAdultMain();
+                dataSet = patientBAL.Dental_GetPatienDropdownData();
                 return Ok(dataSet);
             }
             catch (Exception ex)
